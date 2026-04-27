d4e0e1235bba0336f0094b09a874530cfc525519
ing-brian-dev <ingbvn0@gmail.com>
2026-04-27 12:42:24 -0600

    Add CategoryWithProductsSchema and update getProducts function to parse response

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,3 +1,4 @@
+import { CategoryWithProductsSchema } from "@/app/src/schemas";
 
 type Params = Promise<{ categoryId: string }>
 
@@ -5,13 +6,15 @@ async function getProducts(categoryId: string) {
     const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
     const req = await fetch(url);
     const res = await req.json();
-    
-    return res;
+    const products = CategoryWithProductsSchema.parse(res);
+    return products;
 }
 
 export default async function StorePage({ params }: { params: Params }) {
     const { categoryId } = await params;
-    await getProducts(categoryId);
+    const products = await getProducts(categoryId);
+
+    console.log(products);
 
     return (
         <div>StorePage</div>
---
+++ app/src/schemas.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export const ProductSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    image: z.string(),
+    price: z.coerce.number(),
+    inventory: z.number(),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+    deletedAt: z.coerce.date().nullable(),
+});
+
+export const CategorySchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+    deletedAt: z.coerce.date().nullable(),
+});
+
+export const CategoryWithProductsSchema = CategorySchema.extend({
+    products: z.array(ProductSchema)
+});
