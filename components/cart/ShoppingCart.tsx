59f88303d13824b72f67941ec037cc87008ef7b0
ing-brian-dev <ingbvn0@gmail.com>
2026-04-27 15:13:47 -0600

    Add ShoppingCart component and integrate it into RootLayout; create zustand store for cart management

---
+++ components/cart/ShoppingCart.tsx
@@ -0,0 +1,12 @@
+
+export default function ShoppingCart() {
+  return (
+    <>
+        <h2
+            className="text-4xl font-bold text-shadow-gray-900"
+        >
+            Resumen de Venta
+        </h2>
+    </>
+  )
+}
---
+++ src/store.ts
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+interface Store {
+    total: number;
+}
+
+export const useStore = create<Store>(() => ({
+    total: 0
+}));
