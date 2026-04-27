723c784e4a75fef13a57d28355236276e8e1be70
ing-brian-dev <ingbvn0@gmail.com>
2026-04-27 15:34:51 -0600

    Add AddProductButton component and integrate it into ProductCard; implement addToCart functionality in zustand store

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,13 +1,16 @@
 import { Product } from "@/src/schemas";
 import { formatCurrency } from "@/src/utils";
+import AddProductButton from "./AddProductButton";
 
-export default function ProductCard({ product }: { product: Product }) {  
+export default function ProductCard({ product }: { product: Product }) {
+
+
   return (
     <div
       className='rounded bg-white shadow relative p-5'
     >
       <div>
-        <img 
+        <img
           src={`${process.env.API_URL}/img/${product.image}`}
           alt={`Imagen de Producto ${product.name}`}
           loading="lazy"
@@ -18,14 +21,9 @@ export default function ProductCard({ product }: { product: Product }) {
           <p className="text-2xl font-extrabold  text-gray-900">{formatCurrency(product.price)}</p>
         </div>
       </div>
-      <button
-        type="button"
-        className="absolute top-5 -right-3"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 bg-indigo-600 rounded-full text-white">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </button>
+      <AddProductButton 
+        product={product}
+      />
     </div>
   )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
 import { create } from "zustand";
+import { Product } from "./schemas";
 
 interface Store {
     total: number;
+    addToCart: (product : Product) => void;
 }
 
 export const useStore = create<Store>(() => ({
-    total: 0
+    total: 0,
+    addToCart: (product) => {
+        console.log(product);
+    }
 }));
